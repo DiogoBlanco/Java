93cc9cba2c95d0a7c838a4045cbcbbd9de22b854
DiogoBlanco <diogoblanco@outlook.com.br>
2025-11-04 21:10:51 -0300

    ex_5 intellij

--- ex_aula37/Ex_5.java
+++ ex_aula37/Ex_5.java
@@ -5,30 +5,30 @@ public class Ex_5 {
     public static void main(String[] args) {
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        int código, quantidade;
-        double preço;
+        int codigo, quantidade;
+        double preco;
 
         System.out.println("Digite o código do produto:");
-        código = sc.nextInt();
+        codigo = sc.nextInt();
         System.out.println("Digite a quantidade:");
         quantidade = sc.nextInt();
 
-        if (código == 1) {
-            preço = 4.00 * quantidade;
-        } else if (código == 2) {
-            preço = 4.50 * quantidade;
-        } else if (código == 3) {
-            preço = 5.00 * quantidade;
-        } else if (código == 4) {
-            preço = 2.00 * quantidade;
-        } else if (código == 5) {
-            preço = 1.50 * quantidade;
+        if (codigo == 1) {
+            preco = 4.00 * quantidade;
+        } else if (codigo == 2) {
+            preco = 4.50 * quantidade;
+        } else if (codigo == 3) {
+            preco = 5.00 * quantidade;
+        } else if (codigo == 4) {
+            preco = 2.00 * quantidade;
+        } else if (codigo == 5) {
+            preco = 1.50 * quantidade;
         } else {
-            preço = 0.0;
+            preco = 0.0;
             System.out.println("Código de produto inválido.");
         }
 
-        System.out.printf("Total: R$ %.2f%n", preço);
+        System.out.printf("Total: R$ %.2f%n", preco);
 
         sc.close();
 
