c6b49e31221b0b9324323bfc7f35d7497656a481
DiogoBlanco <diogoblanco@outlook.com.br>
2025-11-04 21:31:39 -0300

    vscode

--- ex_aula37/Ex_6.java
+++ ex_aula37/Ex_6.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Ex_6 {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        double numero;
+        System.out.println("Digite um número qualquer:");
+        numero = sc.nextDouble();
+        if (numero >= 0 && numero <= 25) {
+            System.out.println("Intervalo [0, 25]");
+        }
+        else if (numero > 25 && numero <= 50) {
+            System.out.println("Intervalo: [25, 50]");
+        }
+        else if (numero > 50 && numero <= 75) {
+            System.out.println("Intervalo: [50, 75]");
+        }
+        else if (numero > 75 && numero <= 100) {
+            System.out.println("Intervalo: [75, 100]");
+        }
+        else if (numero < 0 || numero > 100) {
+            System.out.println("Fora do Intervalo");
+        }
+       sc.close();
+    }
 }
