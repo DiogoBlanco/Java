f1bdbb2c3afea3fbfc0905add6b784e66ee42ce3
DiogoBlanco <diogoblanco@outlook.com.br>
2025-11-08 18:02:04 -0300

    aula59

---
+++ aula54/aula54/Ex_7.java
@@ -0,0 +1,19 @@
+package src;
+
+import java.util.Scanner;
+
+public class Ex_7 {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um número inteiro: ");
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            int first = i;
+            int second = i * i;
+            int third = i * i * i;
+            System.out.printf("%d %d %d%n", first, second, third);
+        }
+        sc.close();
+    }
+}
