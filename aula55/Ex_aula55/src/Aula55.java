e150c09ee2881319ba3cd61c80b89b589a93e15f
DiogoBlanco <diogoblanco@outlook.com.br>
2025-11-08 18:02:21 -0300

    aula59

--- aula55/Ex_aula55/src/Aula55.java
+++ aula55/Ex_aula55/src/Aula55.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+void main() {
+    Locale.setDefault(Locale.US);
+    Scanner sc = new Scanner(System.in);
 
-public class Aula55 {
+    char answer;
+
+    do {
+        IO.println("Digite a temperatura em Celsius: ");
+        double temp = sc.nextDouble();
+        double fahrenheit = (9 * temp) / 5 + 32;
+        System.out.printf("Equivalente em Fahreinheit: %.1f%n", fahrenheit);
+        IO.println("Deseja repetir (s/n)?");
+        answer = sc.next().charAt(0);
+    } while (answer != 'n');
+    sc.close();
 }
--- aula59/aula/src/Aula.java
+++ aula59/aula/src/Aula.java
@@ -1,4 +1,30 @@
-package PACKAGE_NAME;
+import java.util.Locale;
 
 public class Aula {
+    public static void main(String[] args) {
+        String original = "abcde FGHIJ ABC abc DEFG    ";
+
+        String s01 = original.toLowerCase();
+        String s02 = original.toUpperCase();
+        String s03 = original.trim();
+        String s04 = original.substring(2);
+        String s05 = original.substring(2, 9);
+        String s06 = original.replace('a', 'x');
+        String s07 = original.replace("abc", "xy");
+        // lastIndexOf() - indexOf()
+
+        System.out.println(s01);
+        System.out.println(s02);
+        System.out.println(s03);
+        System.out.println(s04);
+        System.out.println(s05);
+        System.out.println(s06);
+        System.out.println(s07);
+    }
 }
+
+/*
+
+* Comentário de Bloco
+
+* */
