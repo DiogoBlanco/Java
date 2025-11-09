1c57b184997d002409e83ad75b8674b2b6e4340c
DiogoBlanco <diogoblanco@outlook.com.br>
2025-11-08 21:42:51 -0300

    aula66

--- aula64/aula64/src/application/Aula64.java
+++ aula64/aula64/src/application/Aula64.java
@@ -20,11 +20,8 @@ public class Aula64 {
         y.b = sc.nextDouble();
         y.c = sc.nextDouble();
 
-        double xP = (x.a + x.b + x.c) / 2;
-        double yP = (y.a + y.b + y.c) / 2;
-
-        double xArea = Math.sqrt(xP * (xP - x.a) * (xP - x.b) * (xP - x.c));
-        double yArea = Math.sqrt(yP * (yP - y.a) * (yP - y.b) * (yP - y.c));
+        double xArea = x.area();
+        double yArea = y.area();
 
         System.out.printf("Triangle x area: %.4f%n", xArea);
         System.out.printf("Triangle y area: %.4f%n", yArea);
